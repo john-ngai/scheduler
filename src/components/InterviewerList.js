1d990cbbff9ae9f053659ac4f00b117059849a32
mrjohnming <john.ngai@hotmail.com>
2022-02-23 18:11:43 -0500

    Add array of InterviewerListItem components to HTML

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,12 +1,23 @@
 import React from 'react';
-import classNames from 'classnames';
 import 'components/InterviewerList.scss';
+import InterviewerListItem from 'components/InterviewerListItem';
 
-export default function InterviewerList() {
+export default function InterviewerList(props) {
+  const listItem = props.interviewers.map(interviewer =>
+    <InterviewerListItem
+      key={interviewer.id}
+      id={interviewer.id}
+      name={interviewer.name}
+      avatar={interviewer.avatar}
+      selected={interviewer.id === props.interviewer}
+      setInterviewer={props.setInterviewer}
+    />
+  );
+
   return (
     <section className="interviewers">
       <h4 className="interviewers__header text--light">Interviewer</h4>
-      <ul className="interviewers__list"></ul>
+      <ul className="interviewers__list">{listItem}</ul>
     </section>
   );
 }
