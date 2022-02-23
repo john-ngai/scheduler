412558d5a14a2c98119d982575220b6c9a20c806
mrjohnming <john.ngai@hotmail.com>
2022-02-23 18:38:23 -0500

    Refactor codebase

--- stories/index.js
+++ stories/index.js
@@ -138,6 +138,6 @@ storiesOf("InterviewerList", module)
   .add("Clickable", () => (
     <InterviewerList
       interviewers={interviewers}
-      setInterviewer={action("setInterviewer")}
+      setInterviewer={() => action("setInterviewer")(interviewer.id)}
     />
   ));
