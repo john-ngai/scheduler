1de09cab10cba0310d03e9d7d735661f8df853df
mrjohnming <john.ngai@hotmail.com>
2022-02-24 13:12:39 -0500

    Add props conditionals

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,6 +3,9 @@ import 'components/Appointment/styles.scss';
 
 export default function Appointment(props) {
   return (
-    <article className="appointment"></article>
+    <article className="appointment">
+      {!props.time && <span>No Appointments</span>}
+      {props.time && <span>Appointment at {props.time}</span>}
+    </article>
   );
 }
