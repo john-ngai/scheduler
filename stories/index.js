c779593028785d8b14fb5de71aa9181b932c08cf
mrjohnming <john.ngai@hotmail.com>
2022-02-24 14:00:02 -0500

    Fix bugs

--- stories/index.js
+++ stories/index.js
@@ -14,6 +14,7 @@ import Empty from "components/Appointment/Empty.js";
 import Show from "components/Appointment/Show.js";
 import Confirm from "components/Appointment/Confirm.js";
 import Status from "components/Appointment/Status.js";
+import Error from "components/Appointment/Error.js";
 
 storiesOf("Button", module)
   .addParameters({
