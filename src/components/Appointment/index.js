319c32160a69c3561c3ab20e51e59b94f3760a16
mrjohnming <john.ngai@hotmail.com>
2022-02-24 20:01:48 -0500

    Create appointment display

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import 'components/Appointment/styles.scss';
 import Header from 'components/Appointment/Header.js';
+import Show from 'components/Appointment/Show.js';
+import Empty from 'components/Appointment/Empty.js';
 
 export default function Appointment(props) {
   return (
     <article className="appointment">
-      {!props.time && <span>No Appointments</span>}
-      {props.time && <span>Appointment at {props.time}</span>}
+      <Header time={props.time}/>
+      {props.interview ?
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+        />
+        :
+        <Empty/>}
     </article>
   );
 }
