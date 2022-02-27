46f713f09a4f3751c99026aa19e5558a91eba51c
mrjohnming <john.ngai@hotmail.com>
2022-02-26 23:49:02 -0500

    Create a back function & implement a replace param to the transition function

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,8 +1,28 @@
 import {useState} from 'react';
 
 const useVisualMode = initial => {
-  const [mode, setMode] = useState(initial)
-  const transition = newState => setMode(newState);
-  return {mode, transition}; // Can also be written as {mode: mode}.
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+
+  const transition = (mode, replace = false) => {
+    setMode(mode);
+    if (replace) {
+      history.length > 1 ? history.pop() : null;
+    }
+    setHistory(prev => [...prev, mode]);
+  }
+  
+  const back = () => {
+    if (history.length > 1) {
+      history.pop();
+      setMode(history[history.length - 1]);
+    }
+    if (history.length === 1) {
+      setMode(history[0]);
+    }
+  };
+
+  return {mode, transition, back};
 }
+
 export default useVisualMode;
