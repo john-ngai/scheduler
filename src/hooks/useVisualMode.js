f68ff2b817104f2ffaf88aebce2ca8317b1f04f9
mrjohnming <john.ngai@hotmail.com>
2022-02-26 22:32:43 -0500

    Fix bugs

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,5 +1,7 @@
+import {useState} from 'react';
+
 const useVisualMode = initial => {
-  const [mode, setMode] = useState(intial)
+  const [mode, setMode] = useState(initial)
   return {mode}; // Can also be written as {mode: mode}.
 }
 
