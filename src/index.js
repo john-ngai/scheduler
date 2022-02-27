52b093654e30e402a8faf1b262558f3e79f85218
mrjohnming <john.ngai@hotmail.com>
2022-02-27 00:45:58 -0500

    Fix bugs

--- src/index.js
+++ src/index.js
@@ -3,7 +3,4 @@ import ReactDOM from "react-dom";
 import "index.scss";
 import Application from "components/Application";
 
-const EMPTY = "EMPTY";
-const SHOW = "SHOW";
-
 ReactDOM.render(<Application />, document.getElementById("root"));
