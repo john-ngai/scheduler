8bb9ba1fc68f0b90c09cdb06c6b1e4a9dafbb061
mrjohnming <john.ngai@hotmail.com>
2022-02-27 00:27:17 -0500

    Declare initial mode constants

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
 import "index.scss";
-
 import Application from "components/Application";
+
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
 
 ReactDOM.render(<Application />, document.getElementById("root"));
