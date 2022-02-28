4aec642eb23e2a2a820e476ad835bbf03d7b30f0
mrjohnming <john.ngai@hotmail.com>
2022-02-27 19:51:15 -0500

    Modify implementation for Empty & Show, and add the Form

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,23 +1,38 @@
 import React from 'react';
 import 'components/Appointment/styles.scss';
-import Header from 'components/Appointment/Header.js';
-import Show from 'components/Appointment/Show.js';
-import Empty from 'components/Appointment/Empty.js';
+import Header from 'components/Appointment/Header';
+import Empty from 'components/Appointment/Empty';
+import Show from 'components/Appointment/Show';
+import Form from 'components/Appointment/Form';
+import useVisualMode from 'hooks/useVisualMode';
 
-const EMPTY = "EMPTY";
-const SHOW = "SHOW";
+const EMPTY = 'EMPTY';
+const SHOW = 'SHOW';
+const CREATE = 'CREATE';
 
 export default function Appointment(props) {
+  const { mode, transition, back } = useVisualMode(props.interview ? SHOW : EMPTY);
+
   return (
     <article className="appointment">
-      <Header time={props.time}/>
-      {props.interview ?
+      <Header time={props.time} />
+
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+
+      {mode === SHOW && 
         <Show
           student={props.interview.student}
           interviewer={props.interview.interviewer.name}
         />
-        :
-        <Empty/>}
+        }
+
+      {mode === CREATE && 
+        <Form
+          interviewers={[]} // Temporarily set as an empty array.
+          onSave={() => console.log("onSave")}
+          onCancel={() => back()}
+        />
+      }
     </article>
   );
 }
