2910c00f0b6dac7e7c2379e78f240f9825a1751d
mrjohnming <john.ngai@hotmail.com>
2022-08-02 09:53:06 -0400

    Add reducers - appointments & inteviewers

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import daysReducer from './daysSlice';
+import appointmentsReducer from './appointmentsSlice';
+import interviewersReducer from './interviewersSlice';
 
 export default configureStore({
   reducer: {
     days: daysReducer,
+    appointments: appointmentsReducer,
+    interviewers: interviewersReducer,
   },
 });
