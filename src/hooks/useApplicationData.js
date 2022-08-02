145391ad5af269b20283278540ab32c8bc5b6559
mrjohnming <john.ngai@hotmail.com>
2022-08-02 12:35:18 -0400

    Add comments

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,11 +1,12 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
+// 2Aug2022 @ 12:25 PM: No longer used - pending removal
 export default function useApplicationData() {
   const applicationData = {};
 
   const [state, setState] = useState({
-    day: "Monday",
+    day: 'Monday',
     days: [],
     appointments: {},
     interviewers: {},
@@ -14,10 +15,10 @@ export default function useApplicationData() {
     Promise.all([
       axios.get('/api/days'),
       axios.get('/api/appointments'),
-      axios.get('/api/interviewers')
-    ]).then(all => {
+      axios.get('/api/interviewers'),
+    ]).then((all) => {
       const [days, appointments, interviewers] = all;
-      setState(prev => ({
+      setState((prev) => ({
         ...prev,
         days: days.data,
         appointments: appointments.data,
@@ -27,20 +28,20 @@ export default function useApplicationData() {
   }, []);
   applicationData.state = state;
 
-  const setDay = day => setState({ ...state, day });
+  const setDay = (day) => setState({ ...state, day });
   applicationData.setDay = setDay;
 
   const bookInterview = (id, interview) => {
     // Declare a new appointment with the updated interview key-value data.
     const appointment = {
       ...state.appointments[id],
-      interview: { ...interview }
+      interview: { ...interview },
     };
 
     // Declare a new appointments objec with the updated appointment data.
     const appointments = {
       ...state.appointments,
-      [id]: appointment
+      [id]: appointment,
     };
 
     //// START - Updating Spots Remaining ////
@@ -53,36 +54,39 @@ export default function useApplicationData() {
     const selectedDay = state.day;
 
     // Get the appointment IDs for the selected day.
-    days.forEach(day => {
+    days.forEach((day) => {
       if (day.name === selectedDay) {
         index = day.id - 1;
         appointmentIDs = day.appointments;
       }
-    })
+    });
 
     // Increment spots when the interview key-value for the selected appointment id is falsey (null).
-    appointmentIDs.forEach(id => !appointments[id].interview ? spots++ : null);
+    appointmentIDs.forEach((id) =>
+      !appointments[id].interview ? spots++ : null
+    );
 
     // Update the remaining spots for the selected day.
     days[index] = { ...days[index], spots };
     //// END - Updating Spots Remaining ////
 
-    return axios.put(`/api/appointments/${id}`, appointment)
-      .then(() => setState({ ...state, appointments, days }))
-  }
+    return axios
+      .put(`/api/appointments/${id}`, appointment)
+      .then(() => setState({ ...state, appointments, days }));
+  };
   applicationData.bookInterview = bookInterview;
 
-  const cancelInterview = id => {
+  const cancelInterview = (id) => {
     // Declare a new appointment with the updated interview key-value data.
     const appointment = {
       ...state.appointments[id],
-      interview: null
+      interview: null,
     };
 
     // Declare a new appointments objec with the updated appointment data.
     const appointments = {
       ...state.appointments,
-      [id]: appointment
+      [id]: appointment,
     };
 
     //// START - Updating Spots Remaining ////
@@ -95,25 +99,26 @@ export default function useApplicationData() {
     const selectedDay = state.day;
 
     // Get the appointment IDs for the selected day.
-    days.forEach(day => {
+    days.forEach((day) => {
       if (day.name === selectedDay) {
         index = day.id - 1;
         appointmentIDs = day.appointments;
       }
-    })
+    });
 
     // Increment spots when the interview key-value for the selected appointment id is falsey (null).
-    appointmentIDs.forEach(id => !appointments[id].interview ? spots++ : null);
+    appointmentIDs.forEach((id) =>
+      !appointments[id].interview ? spots++ : null
+    );
 
     // Update the remaining spots for the selected day.
     days[index] = { ...days[index], spots };
     //// END - Updating Spots Remaining ////
 
-    return axios.delete(`/api/appointments/${id}`)
-      .then(() => {
-        setState({ ...state, appointments, days });
-      });
-  }
+    return axios.delete(`/api/appointments/${id}`).then(() => {
+      setState({ ...state, appointments, days });
+    });
+  };
   applicationData.cancelInterview = cancelInterview;
 
   return applicationData;
