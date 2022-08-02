0f995d2aa1799453c28213de11e47a11a05775ae
mrjohnming <john.ngai@hotmail.com>
2022-08-01 21:16:13 -0400

    Add Redux store to root component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "index.scss";
-import Application from "components/Application";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Application from 'components/Application';
+import { Provider } from 'react-redux';
+import store from './app/store';
+import 'index.scss';
 
-ReactDOM.render(<Application />, document.getElementById("root"));
+ReactDOM.render(
+  <Provider store={store}>
+    <Application />
+  </Provider>,
+  document.getElementById('root')
+);
