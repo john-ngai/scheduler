b28f1c0d08131a77f70dd7a097ddd2bc9372c03d
mrjohnming <john.ngai@hotmail.com>
2022-08-02 11:45:28 -0400

    Create selector - selectAppointmentsByDay

--- src/app/appointmentsSlice.js
+++ src/app/appointmentsSlice.js
@@ -186,3 +186,34 @@ const appointmentsSlice = createSlice({
 export const { interviewAdded } = appointmentsSlice.actions;
 
 export default appointmentsSlice.reducer;
+
+// **REMINDER**: Refactor with params (state, appointmentIds)
+export const selectAppointmentsByDay = (state) => {
+  const allAppointments = state.appointments;
+  const selectedAppointments = [];
+  const daysList = state.days.daysList;
+  const selectedDay = state.days.selectedDay;
+
+  // If the daysList array is empty, return the empty selectedAppointments array.
+  if (daysList.length === 0) {
+    return selectedAppointments;
+  }
+
+  if (daysList.length > 0) {
+    daysList.forEach((day) => {
+      if (day.name === selectedDay) {
+        // Store the array of appointment ids for the selected day.
+        const appointmentIds = day.appointments;
+
+        // Push each matching appointment object into the selectedAppointments array.
+        appointmentIds.forEach((id) =>
+          selectedAppointments.push(allAppointments[id])
+        );
+        return selectedAppointments;
+      }
+    });
+  }
+
+  // Return an empty selectedAppointments array if no day is selected.
+  return selectedAppointments;
+};
