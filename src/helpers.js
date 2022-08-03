09317377d5ef932e3e3f60083f7e8c286aab24a0
mrjohnming <john.ngai@hotmail.com>
2022-08-03 11:56:35 -0400

    Create helper function - formatInterview

--- src/helpers.js
+++ src/helpers.js
@@ -17,4 +17,21 @@ export const isStateLoaded = (state) => {
   }
 
   return true;
-}
+};
+
+/**
+ * Returns a formatted interview argument:
+ *  from  { student: String, interviewer: Number }
+ *  to    { student: String, interviewer: { id, name, avatar } }
+ */
+export const formatInterview = (state, interview) => {
+  // If no interview is booked (null), return null.
+  if (!interview) {
+    return null;
+  }
+
+  const interviewers = state.interviewers;
+  const selectedInterviewerId = interview.interviewer;
+
+  return { ...interview, interviewer: interviewers[selectedInterviewerId] };
+};
