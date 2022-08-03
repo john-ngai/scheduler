4431b7535c51f006c2d6bdc8c953b5d2cfe1aa3d
mrjohnming <john.ngai@hotmail.com>
2022-08-03 12:16:00 -0400

    Refactor selector function - selectInterviewersByDay

--- src/app/interviewersSlice.js
+++ src/app/interviewersSlice.js
@@ -22,33 +22,13 @@ const interviewersSlice = createSlice({
 
 export default interviewersSlice.reducer;
 
-// Refactor with params (state, interviewerIds)
-export const selectInterviewersByDay = (state) => {
+// Given the state & array of interviewer ids for the selected day,
+// return an array of the complete interviewer objects.
+export const selectInterviewersByDay = (state, interviewerIds) => {
   const allInterviewers = state.interviewers;
   const selectedInterviewers = [];
-  const daysList = state.days.daysList;
-  const selectedDay = state.days.selectedDay;
-
-  // If the daysList array is empty, return the empty selectedInterviewers array.
-  if (daysList.length === 0) {
-    return selectedInterviewers;
-  }
-
-  if (daysList.length > 0) {
-    daysList.forEach((day) => {
-      if (day.name === selectedDay) {
-        // Store the array of interviewer ids for the selected day.
-        const interviewerIds = day.interviewers;
-
-        // Push each matching interviewer object into the selectedInterviewers array.
-        interviewerIds.forEach((id) =>
-          selectedInterviewers.push(allInterviewers[id])
-        );
-        return selectedInterviewers;
-      }
-    });
-  }
-
-  // Return an empty selectedInterviewers array if no day is selected.
+  interviewerIds.forEach((id) => {
+    selectedInterviewers.push(allInterviewers[id]);
+  });
   return selectedInterviewers;
 };
