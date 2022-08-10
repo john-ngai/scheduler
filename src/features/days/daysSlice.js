70b14323b64593ff61695563390e6dd2ae0bfd9f
mrjohnming <john.ngai@hotmail.com>
2022-08-10 14:44:24 -0400

    Replace selector function - selectDayListItemBySelectedDay with selectDayEntityBySelectedDay

--- src/features/days/daysSlice.js
+++ src/features/days/daysSlice.js
@@ -60,34 +60,28 @@ export default daysSlice.reducer;
   selectById: given the state and an entity ID, returns the entity with that ID or undefined.
 */
 
-// Customized selectors for the daysAdapter.
 export const {
-  selectAll: selectAllDays, // ** Used in DayList.js, helpers.js, & AppointmentsList.js ** - Returns an array of all the entities.
-  selectById: selectDayById, // Given (state, id), returns the entity with that id or undefined.
-  selectIds: selectDayIds, // Returns an array of all the ids.
+  selectAll: selectAllDays, // Returns an array of all the day entities.
 } = daysAdapter.getSelectors((state) => state.days);
 
-export const selectDayListItemBySelectedDay = (state) => {
-  // const daysList = state.days.daysList;
-  const allDays = selectAllDays(state);
-  const selectedDay = state.days.selectedDay;
-  const dayListItem = allDays.find((day) => day.name === selectedDay);
-  return dayListItem;
+export const selectSelectedDay = (state) => state.days.selectedDay;
+
+export const selectDayEntityBySelectedDay = (allDays, selectedDay) => {
+  const day = allDays.find((day) => day.name === selectedDay);
+  return day;
 };
 
 export const selectAppointmentIdsBySelectedDay = (allDays, selectedDay) => {
-  const day = allDays.find(day => day.name === selectedDay);
+  const day = allDays.find((day) => day.name === selectedDay);
   return day.appointments;
-}
+};
 
 export const selectInterviewerIdsBySelectedDay = (allDays, selectedDay) => {
-  const day = allDays.find(day => day.name === selectedDay);
+  const day = allDays.find((day) => day.name === selectedDay);
   return day.interviewers;
-}
+};
 
 export const selectSpotsBySelectedDay = (allDays, selectedDay) => {
-  const day = allDays.find(day => day.name === selectedDay);
+  const day = allDays.find((day) => day.name === selectedDay);
   return day.spots;
-}
-
-export const selectSelectedDay = state => state.days.selectedDay;
+};
