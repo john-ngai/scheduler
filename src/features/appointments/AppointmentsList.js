12167e74e294ed7a35992038bd3860d7b55b58cc
mrjohnming <john.ngai@hotmail.com>
2022-08-10 10:08:34 -0400

    Refactor selectAppointmentIdsBySelectedDay to use more specific parameters

--- src/features/appointments/AppointmentsList.js
+++ src/features/appointments/AppointmentsList.js
@@ -1,42 +1,30 @@
 // React
-import React from 'react'
+import React from 'react';
 import AppointmentListItem from './AppointmentListItem';
-// Helper functions
-import { formatInterview, isStateLoaded } from 'helpers/helpers';
 // Redux
 import { useSelector } from 'react-redux';
-import { selectAppointmentsBySelectedDay } from './appointmentsSlice';
-import { selectInterviewersByDay } from 'features/interviewers/interviewersSlice';
-import { selectInterviewerIdsBySelectedDay } from 'features/days/daysSlice';
+import {
+  selectAllDays,
+  selectSelectedDay,
+  selectAppointmentIdsBySelectedDay,
+} from 'features/days/daysSlice';
 
 export default function AppointmentsList() {
-  const state = useSelector((state) => state);
+  const allDays = useSelector(selectAllDays);
+  const selectedDay = useSelector(selectSelectedDay);
+  const appointmentIds = selectAppointmentIdsBySelectedDay(
+    allDays,
+    selectedDay
+  );
 
-  let renderedAppointments = null;
+  const renderedAppointments = appointmentIds.map((id) => (
+    <AppointmentListItem key={id} appointmentId={id} />
+  ));
 
-  // Create the renderedAppointments after successfully
-  // fetching & setting the state from the API.
-  if (isStateLoaded(state)) {
-    const appointments = selectAppointmentsBySelectedDay(state);
-    renderedAppointments = appointments.map((appointment) => (
-      <AppointmentListItem
-        key={appointment.id}
-        id={appointment.id}
-        time={appointment.time}
-        interview={formatInterview(state, appointment.interview)}
-        interviewers={selectInterviewersByDay(
-          state,
-          selectInterviewerIdsBySelectedDay(state)
-        )}
-      />
-    ));
-  }
   return (
     <section className="schedule">
       {renderedAppointments}
-      {isStateLoaded(state) ? (
-        <AppointmentListItem key="last" id="last" time="5pm" />
-      ) : null}
+      <AppointmentListItem key="last" appointmentId="last" time="5pm" />
     </section>
   );
 }
