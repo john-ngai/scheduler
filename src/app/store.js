b7efee9a2b83502bd97d8d8117b76c7e4d145cf6
mrjohnming <john.ngai@hotmail.com>
2022-08-10 16:13:00 -0400

    Arrange reducers alphabetically

--- src/app/store.js
+++ src/app/store.js
@@ -1,12 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
-import daysReducer from '../features/days/daysSlice';
 import appointmentsReducer from '../features/appointments/appointmentsSlice';
+import daysReducer from '../features/days/daysSlice';
 import interviewersReducer from '../features/interviewers/interviewersSlice';
 
 export default configureStore({
   reducer: {
-    days: daysReducer,
     appointments: appointmentsReducer,
+    days: daysReducer,
     interviewers: interviewersReducer,
   },
 });
